27f69248b6a786d4bd8801009031557059cc966c
krank666 <emosick@qq.com>
2023-09-05 04:15:04 +0800

    数据类型转换

--- main.go
+++ main.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
-
 /*
 	func main() {
 		var num1 uint
@@ -34,13 +29,33 @@ import (
 
 // 数据类型转换
 func main() {
-	var string1 string
-	string1 = "123"
-	int1, _ := strconv.Atoi(string1) // string to int
-	fmt.Printf("%T", int1)           // 查看类型
-	fmt.Println(int1)                // 查看打印结果
 
-	int2, _ := strconv.ParseInt(string1, 10, 8) //string to int64
-	// strconv.ParseInt需要三个参数 1. 需要转化的string值 2. 转化后所需要的进制，2~32进制 3. 限制位 这个包只能输出int64的类型
-	fmt.Printf("%T", int2)
+	/*
+		// string to int
+		var string1 string
+		string1 = "123"
+		int1, _ := strconv.Atoi(string1)
+		fmt.Printf("%T", int1)           // 查看类型
+		fmt.Println(int1)                // 查看打印结果
+	*/
+	/*	int2, _ := strconv.ParseInt(string1, 10, 8) //string to int64
+		// strconv.ParseInt需要三个参数 1. 需要转化的string值 2. 转化后所需要的进制，2~32进制 3. 限制位 这个包只能输出int64的类型
+		fmt.Printf("%T", int2)*/
+
+	/*	var num1 int = 123
+		string2 := strconv.Itoa(num1)
+		fmt.Printf("%T", string2)*/
+
+	/*	// int64转string
+		var num0 int64 = 1134
+		string0 := strconv.FormatInt(num0, 10)
+		fmt.Printf("%T", string0)*/
+
+	/*	// string转float32/64
+		floatNumb, err := strconv.ParseFloat("123.3412312312", 64)
+		fmt.Println(floatNumb)
+		fmt.Println(err)*/
+
+	// int64 转 int
+
 }
